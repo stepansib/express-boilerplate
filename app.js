704dd8fa5e0bd01622fc861fd3ec678e33f76dbb
Stepan Yudin <stepan.sib@gmail.com>
2020-06-29 22:37:47 +0300

    Knex and DB connection configured

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var knex = require('knex')({
+  client: 'mysql2',
+  connection: {
+    host : process.env.DB_HOST,
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    database : process.env.DB_NAME
+  }
+});
 
 // Process env variables
 require('dotenv').config();
