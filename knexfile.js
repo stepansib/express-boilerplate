65124cab1312583c1976f8cc21f2689ac4ac9d56
Stepan Yudin <stepan.sib@gmail.com>
2020-06-30 19:58:26 +0300

    Added knex config

---
+++ knexfile.js
@@ -0,0 +1,62 @@
+// Update with your config settings.
+
+// Process env variables
+require('dotenv').config();
+
+module.exports = {
+
+  development: {
+    client: 'mysql2',
+    connection: {
+      host : process.env.DB_HOST,
+      user : process.env.DB_USER,
+      password : process.env.DB_PASSWORD,
+      database : process.env.DB_NAME,
+      charset: 'utf8'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'migrations'
+    }
+  },
+
+  staging: {
+    client: 'mysql2',
+    connection: {
+      host : process.env.DB_HOST,
+      user : process.env.DB_USER,
+      password : process.env.DB_PASSWORD,
+      database : process.env.DB_NAME,
+      charset: 'utf8'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'migrations'
+    }
+  },
+
+  production: {
+    client: 'mysql2',
+    connection: {
+      host : process.env.DB_HOST,
+      user : process.env.DB_USER,
+      password : process.env.DB_PASSWORD,
+      database : process.env.DB_NAME,
+      charset: 'utf8'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'migrations'
+    }
+  }
+
+};
