d34da1307840a9b63213028bcc820a6352879157
Stepan Yudin <stepan.sib@gmail.com>
2020-06-30 20:30:50 +0300

    Updated persons route

--- routes/persons.js
+++ routes/persons.js
@@ -2,19 +2,23 @@ var express = require('express');
 var router = express.Router();
 var Person = require('../models/Person');
 var _ = require('lodash');
+var {NotFound, BadRequest} = require('../errors/errors');
 
-/* GET users listing. */
 router.get('/', async function (req, res, next) {
     try {
-        const person = await Person.query().findById(1);
+        const person = await Person.query().findById(req.body.id);
         if (_.isUndefined(person)) {
-            throw new Error('Person not found');
+            throw new NotFound('Person not found');
         }
-        res.status(200).send(person);
+        res.status(200).json(person);
     }
     catch(error) {
         next(error);
     }
+});
+
+router.post('/create', async function (req, res, next) {
+
 });
 
 module.exports = router;
