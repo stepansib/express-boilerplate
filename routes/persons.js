521e99a5e6be6df1707e498d8160d965f21c8883
Stepan Yudin <stepan.sib@gmail.com>
2020-07-01 17:18:55 +0300

    More person routes

--- routes/persons.js
+++ routes/persons.js
@@ -5,6 +5,18 @@ var _ = require('lodash');
 var {NotFound, BadRequest} = require('../errors/errors');
 var HttpStatus = require('http-status-codes');
 
+router.get('/all', async function (req, res, next) {
+    try {
+        const persons = await Person.query();
+        // if (_.isUndefined(person)) {
+        //     throw new NotFound('Person not found');
+        // }
+        res.status(HttpStatus.OK).json(persons);
+    } catch (error) {
+        next(error);
+    }
+});
+
 router.get('/', async function (req, res, next) {
     try {
         const person = await Person.query().findById(req.body.id);
@@ -12,14 +24,32 @@ router.get('/', async function (req, res, next) {
             throw new NotFound('Person not found');
         }
         res.status(HttpStatus.OK).json(person);
-    }
-    catch(error) {
+    } catch (error) {
         next(error);
     }
 });
 
 router.post('/create', async function (req, res, next) {
+    try {
+        const newPerson = Person.fromJson(req.body);
+        await Person.query().insert(newPerson);
+        res.status(HttpStatus.CREATED).json(newPerson);
+    } catch (error) {
+        next(error);
+    }
+});
 
+router.delete('/delete', async function (req, res, next) {
+    try {
+        const person = await Person.query().findById(req.body.id);
+        if (_.isUndefined(person)) {
+            throw new NotFound('Person not found');
+        }
+        await Person.query().deleteById(person.id)
+        res.sendStatus(HttpStatus.OK);
+    } catch (error) {
+        next(error);
+    }
 });
 
 module.exports = router;
