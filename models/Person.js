9db00bf896010489975b20b6fe761d8b83afc5d8
Stepan Yudin <stepan.sib@gmail.com>
2020-07-01 17:18:42 +0300

    Simple validation

--- models/Person.js
+++ models/Person.js
@@ -13,6 +13,21 @@ class Person extends Model {
     fullName() {
         return this.firstName + ' ' + this.lastName;
     }
+
+    static get jsonSchema() {
+        return {
+            type: 'object',
+            required: ['firstName', 'lastName'],
+
+            properties: {
+                id: { type: 'integer' },
+                //parentId: { type: ['integer', 'null'] },
+                firstName: { type: 'string', minLength: 2, maxLength: 255 },
+                lastName: { type: 'string', minLength: 2, maxLength: 255 },
+                email: { type: 'string', minLength: 6 },
+            }
+        };
+    }
 }
 
 module.exports = Person;
