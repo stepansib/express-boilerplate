c653edf37cfa648f917acf8735e30aeb943cf7bd
Stepan Yudin <stepan.sib@gmail.com>
2020-07-02 13:08:05 +0300

    Fixed validation method name

--- models/Company.js
+++ models/Company.js
@@ -11,10 +11,10 @@ class Company extends Model {
     }
 
     $beforeInsert(queryContext) {
-        return this.validateEmail();
+        return this.validateName();
     }
 
-    validateEmail() {
+    validateName() {
         return new Promise((resolve, reject) => {
             Company.query().where("name", this.name).then((companies, error) => {
                 if (companies.length === 0) {
